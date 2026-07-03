f2bb636e246f52494e4eb20e718d8da029255af1
97z9s6bdvc-ctrl <97z9s6bdvc@privaterelay.appleid.com>
2026-07-03 13:30:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+// =========================
+// P Test Ver.0.2
+// =========================
+
+let balls = 250;
+let loan = 1;
+
+// 打ち出し
+let firing = false;
+let fireTimer = null;
+
+// 保留
+let reserve = 0;
+const MAX_RESERVE = 4;
+
+// 釘
+const needleTable = [
+    {name:"今日はかなり渋そう…", rate:0.035},
+    {name:"今日は少し渋いかも", rate:0.045},
+    {name:"今日は普通かな", rate:0.055},
+    {name:"今日は回りそう！", rate:0.063},
+    {name:"今日は神釘だ！", rate:0.070}
+];
+
+const needle =
+    needleTable[
+        Math.floor(Math.random()*needleTable.length)
+    ];
+
+document.getElementById("needleMessage").innerText =
+needle.name;
+
+// ------------------------
+
+updateScreen();
+
+// ------------------------
+
+document
+.getElementById("startButton")
+.addEventListener("click", start);
+
+document
+.getElementById("stopButton")
+.addEventListener("click", stop);
+
+// ------------------------
+
+function start(){
+
+    if(firing) return;
+
+    firing = true;
+
+    document.getElementById("message").innerText =
+    "打ち出し中";
+
+    fireTimer = setInterval(fireBall,600);
+
+}
+
+// ------------------------
+
+function stop(){
+
+    firing = false;
+
+    clearInterval(fireTimer);
+
+    document.getElementById("message").innerText =
+    "停止";
+
+}
+
+// ------------------------
+
+function fireBall(){
+
+    if(balls<=0){
+
+        balls += 250;
+        loan++;
+
+    }
+
+    balls--;
+
+    animateBall();
+
+    updateScreen();
+
+}
+
+// ------------------------
+
+function animateBall(){
+
+    const ball =
+    document.getElementById("ball");
+
+    ball.style.transition = "none";
+    ball.style.top = "10px";
+
+    setTimeout(()=>{
+
+        ball.style.transition =
+        "top .35s linear";
+
+        ball.style.top = "120px";
+
+    },20);
+
+    setTimeout(()=>{
+
+        if(Math.random()<needle.rate){
+
+            enterHeso();
+
+        }
+
+    },360);
+
+}
+
+// ------------------------
+
+function enterHeso(){
+
+    if(reserve>=MAX_RESERVE){
+
+        document.getElementById("message").innerText =
+        "保留満タン";
+
+        return;
+
+    }
+
+    reserve++;
+
+    updateReserve();
+
+    document.getElementById("message").innerText =
+    "✨ヘソ入賞！";
+
+}
+
+// ------------------------
+
+function updateReserve(){
+
+    const slots =
+    document.querySelectorAll(".reserveSlot");
+
+    slots.forEach((slot,index)=>{
+
+        if(index<reserve){
+
+            slot.style.background = "white";
+
+        }else{
+
+            slot.style.background = "#444";
+
+        }
+
+    });
+
+}
+
+// ------------------------
+
+function updateScreen(){
+
+    document.getElementById("balls").innerText =
+    balls;
+
+    document.getElementById("loan").innerText =
+    loan;
+
+}
